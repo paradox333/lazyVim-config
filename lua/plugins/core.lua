f71f94d5112fdd05fa0e515618e966a39218d182
paradox333 <leo.yuseff.g@gmail.com>
2024-12-23 13:24:57 -0300

    FIX: se actualiza lazy y configuracion dashboard

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,9 +1,11 @@
 return {
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function()
-      local logo = [[ 
+    "snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[ 
                                                                                   
 ▀███▀▀▀██▄      ██     ▀███▀▀▀██▄       ██     ▀███▀▀▀██▄   ▄▄█▀▀██▄ ▀███▀   ▀██▀ 
   ██   ▀██▄    ▄██▄      ██   ▀██▄     ▄██▄      ██    ▀██▄██▀    ▀██▄ ███▄  ▄█   
@@ -12,57 +14,36 @@ return {
   ██         ████████    ██  ██▄     ████████    ██     ▄███▄      ▄██  ▄█▀▀██▄   
   ██        █▀      ██   ██   ▀██▄  █▀      ██   ██    ▄██▀██▄    ▄██▀ ▄█   ▀██▄  
 ▄████▄    ▄███▄   ▄████▄████▄ ▄███▄███▄   ▄████▄████████▀   ▀▀████▀▀ ▄██▄▄  ▄▄███▄
-                                                                                          
-    ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-      local opts = {
-        theme = "doom",
-        hide = {
-          -- this is taken care of by lualine
-          -- enabling this messes up the actual laststatus setting after loading a file
-          statusline = false,
-        },
-        config = {
-          header = vim.split(logo, "\n"),
+                                                                                  
+    ]],
         -- stylua: ignore
-        center = {
-          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+        ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = "󰀫 ", key = "n", desc = "Nuevo Archivo", action = ":ene | startinsert" },
+            { icon = "󱃮 ", key = "r", desc = "Archivo Reciente", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = "󰇂 ", key = "c", desc = "Configuración", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = "󰘧 ", key = "s", desc = "Restaurar Última Sesión", section = "session" },
+            { icon = "󰏉 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          },
         },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
 
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
-
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "DashboardLoaded",
+        vim.api.nvim_create_autocmd("ColorScheme", {
           callback = function()
-            require("lazy").show()
+            vim.api.nvim_set_hl(0, "SnacksDashboardHeader", {
+              fg = "#1ddd33",
+              bold = false,
+            })
+            vim.api.nvim_set_hl(0, "SnacksDashboardIcon", {
+              fg = "#1ddd33",
+              bold = false,
+            })
+            vim.api.nvim_set_hl(0, "SnacksDashboardDesc", {
+              fg = "#ab5dee",
+              bold = false,
+            })
           end,
-        })
-      end
-
-      return opts
-    end,
+        }),
+      },
+    },
   },
 }
